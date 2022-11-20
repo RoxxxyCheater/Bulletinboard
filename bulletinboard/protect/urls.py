0dfee376ee4e400450c619167559b7a8f94a7b6b
@RoxxxyCheater <71335149+RoxxxyCheater@users.noreply.github.com>
2022-11-20 16:50:32 +0200

    newsletters html form

--- bulletinboard/protect/urls.py
+++ bulletinboard/protect/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import upgrade_me, IndexView
+
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('upgrade/', upgrade_me, name = 'upgrade'),
 ]
--- bulletinboard/protect/views.py
+++ bulletinboard/protect/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -10,3 +10,7 @@ class IndexView(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         context['is_staff'] = self.request.user.is_superuser
         return context
+
+def upgrade_me(request):
+    print('!!!! SUCCESS !!!!')
+    return redirect('/')
