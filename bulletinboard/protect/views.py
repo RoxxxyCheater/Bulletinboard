44cea69341a7126cdb44f82bd72f0e3896a2652d
@RoxxxyCheater <71335149+RoxxxyCheater@users.noreply.github.com>
2022-12-02 05:06:48 +0200

    mail send_manual - complited

--- bulletinboard/protect/views.py
+++ bulletinboard/protect/views.py
@@ -5,31 +5,23 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.core.mail import EmailMultiAlternatives
+from ignore import DEFAULT_FROM_EMAIL
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'flatpages/default.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['is_staff'] = self.request.user.is_superuser
-        return context
-
-    def post(self, *args, **kwargs):
-        print(self, *args, **kwargs)
-        print('!!!! BIG SUCCESS !!!!')
-        user = self.request.user
-        print(user, self.request.user.username)
-        return upgrade_me(self, user, *args, **kwargs)
 
 @login_required
-
 def upgrade_me(self):
     print('!!!! SUCCESS !!!!')
-    for user in User.objects.all():
-        users_i_email = self.user.email#user.objects.filter('email').values()
-        if users_i_email != user.email:
-            # users = users_i_email.get('newsletters_form_request')
-            #print(user, users)
-            msg = EmailMultiAlternatives(
 
+    for user in User.objects.all():
+        users_i_email = self.user.email
+        if users_i_email != user.email:
+            msg = EmailMultiAlternatives(
+                subject= self.POST.get('title_email'),
+                body=self.POST.get('content_email'),
+                from_email= DEFAULT_FROM_EMAIL,
+                to=[user.email],
             )
-    return HttpResponse(f"<h2>Name: {user},{users}")
+            msg.send()
+    return HttpResponse(f"<h2>Email sending was successful!</h2>Press <a href='http://127.0.0.1:8000/callboard/'>here</a> to main page")
